90384091383c1e9add60cd96073b9154ee21d13d
hoparkgo9ma <me@ho9.me>
2018-07-01 18:40:00 +0900

    Update installation test code

--- app.py
+++ app.py
@@ -8,3 +8,18 @@ try:
     import LocalSettings
 except:
     installation.start()
+
+dictionary = open('dic.json', 'r', encoding = 'utf-8').read()
+webview = open('view/index.html', 'r', encoding = 'utf-8').read()
+
+@app.route('/')
+def main():
+    publish_unit = '공개 범위 : '
+    if LocalSettings.ENTREE_HOST_PUBLISH == 'local':
+        publish_unit += '로컬'
+    elif LocalSettings.ENTREE_HOST_PUBLISH == 'server':
+        publish_unit += '서버'
+    else:
+        return 0
+
+app.run(LocalSettings.FLASK_HOST, LocalSettings.FLASK_HOST_PORT)
--- installation.py
+++ installation.py
@@ -1,15 +1,20 @@
 import random
 from flask import Flask
+
 app = Flask(__name__)
 
-content = ''
+webview = open('view/installation.html', 'r', encoding = 'utf-8').read()
+
+@app.route('/')
+def main():
+    webview
+
+@app.route('/', methods=['POST'])
+def install():
+    webview
 
 def start():
     flask_port_tmp = ''
     for i in range(5):
         flask_port_tmp += str(random.randrange(0, 9))
     app.run('localhost', int(flask_port_tmp))
-
-@app.route('/')
-def installation():
-    return content
