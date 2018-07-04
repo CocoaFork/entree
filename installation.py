5c5a8ab8a50fa9cdf615ec6200bc6cbfa0065ae6
hoparkgo9ma <me@ho9.me>
2018-07-04 18:54:36 +0900

    update installation

--- installation.py
+++ installation.py
@@ -17,11 +17,20 @@ def install():
     FLASK_HOST_PORT = request.form['flask-host-port']
     CRYPT_SECRET_KEY = request.form['crypt-secret-key']
     if post_verify == verifiation_code:
-        #아래 작동 안함
-        LocalSettingsValue = open('LocalSettings-example.py').read()
-        LocalSettingsValue.replace("SQLITE3_FILENAME = 'db.db'", "SQLITE3_FILENAME = '"+SQLITE3_FILENAME+"'")
-        LocalSettingsValue.replace("FLASK_HOST_PORT = 5000", "FLASK_HOST_PORT = "+FLASK_HOST_PORT)
-        LocalSettingsValue.replace("CRYPT_SECRET_KEY = 'This is a secret key.'", "CRYPT_SECRET_KEY = '"+CRYPT_SECRET_KEY+"'")
+        LocalSettingsValue = open('LocalSettings-template.py').read()
+        LocalSettingsValue = LocalSettingsValue.replace('<SQLITE3_FILENAME>', SQLITE3_FILENAME)
+        LocalSettingsValue = LocalSettingsValue.replace('<SQLITE3_NO_DB_IGNORE>', 'error_page')
+        LocalSettingsValue = LocalSettingsValue.replace('<FLASK_HOST>', 'localhost')
+        LocalSettingsValue = LocalSettingsValue.replace('<FLASK_HOST_PORT>', FLASK_HOST_PORT)
+        LocalSettingsValue = LocalSettingsValue.replace('<CRYPT_SECRET_KEY>', CRYPT_SECRET_KEY)
+        LocalSettingsValue = LocalSettingsValue.replace('<ENTREE_APPNAME>', 'ENTREE')
+        LocalSettingsValue = LocalSettingsValue.replace('<ENTREE_LANGUAGE>', 'ko-KR')
+        LocalSettingsValue = LocalSettingsValue.replace('<ENTREE_ENTREE_DEVICE_TYPE>', 'host')
+        LocalSettingsValue = LocalSettingsValue.replace('<ENTREE_GETHOST>', 'https://github.com/kpjhg0124/entree')
+        LocalSettingsValue = LocalSettingsValue.replace('<ENTREE_LICENSE_AGREE>', 'True')
+        LocalSettingsValue = LocalSettingsValue.replace('<ENTREE_HOST_PUBLISH>', 'local')
+        LocalSettingsValue = LocalSettingsValue.replace('<ENTREE_CLIENT_HOST>', '192.168.55.176:5000')
+        LocalSettingsValue = LocalSettingsValue.replace('<ENTREE_CLIENT_HOST_SECRET_KEY>', CRYPT_SECRET_KEY)
         with open('LocalSettings.py', 'w') as f:
             f.write(LocalSettingsValue)
         return LocalSettingsValue
